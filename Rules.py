6713edb560ae69d376c2dc94784f55e9695d8efc
rextremistz <rextremistz@gmail.com>
2020-09-16 22:58:09 +0800

    Initial commit

---
+++ Rules.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
