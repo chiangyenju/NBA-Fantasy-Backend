f5dc7b23e57f25e09d64b2f52ca3f03194f43dd3
rextremistz <rextremistz@gmail.com>
2020-09-19 08:28:44 +0800

    Update PlayerStats.py

--- PlayerStats.py
+++ PlayerStats.py
@@ -8,13 +8,18 @@ Created on Thu Sep 17 08:15:08 2020
 %reset -f
 %clear
 
+import pandas as pd
+
 from nba_api.stats.static import players
 player_list = players.get_players()
 
 
 player_active_list = [player for player in player_list if player['is_active'] == True]
+player_id = player_active_list['id']
 
-
+#stats
+from nba_api.stats.endpoints import  playergamelog
+from nba_api.stats.library.parameters import SeasonAll
 
 
         
